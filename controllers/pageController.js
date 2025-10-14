381814c86a07bf5630bbe9a0d9beb2a21131ef98
Luiz Luna <luizluna.dev@gmail.com>
2025-10-14 11:15:14 -0300

    Criando Controllers

---
+++ controllers/pageController.js
@@ -0,0 +1,27 @@
+exports.getHomePage = (req, res) => {
+    res.render('index');
+}
+
+exports.getFaqPage = (req, res) => {
+    res.render('faq');
+}
+
+exports.getLoginPage = (req, res) => {
+    res.render('login');
+}
+
+exports.getRegisterPage = (req, res) => {
+    res.render('register');
+}
+
+exports.getImageToPdfPage = (req, res) => {
+    res.render('image-to-pdf');
+}
+
+exports.getCombinarPdf = (req, res) => {
+    res.render('combinar-pdf');
+}
+
+exports.getFaqPage = (req, res) => {
+    res.render('faq');
+}
