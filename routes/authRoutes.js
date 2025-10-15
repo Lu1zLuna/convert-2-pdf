6aa01c222c32a9f27330a696935a994215770f89
Luiz Luna <luizluna.dev@gmail.com>
2025-10-15 12:53:13 -0300

    Iniciar implementação das rotas de autenticação

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,16 @@
-// authRoutes ainda não implementado
+const express = require('express');
+const router = express.Router();
+
+// Importa o authController
+const authController = require('../controllers/authController');
+
+// Define as rotas de autenticação:
+
+// o formulário de registro enviará os dados para esta rota
+router.post('login', authController.registerUser);
+
+// o formulário de login enviará os dados para esta rota
+
+//  rota para a página de acesso ao painel de usuário
+
+// rota de logout
