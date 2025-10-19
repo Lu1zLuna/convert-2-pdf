f260e3a91fd89c9da00c48e1a878fd4f54689e02
Luiz Luna <luizluna.dev@gmail.com>
2025-10-19 17:00:50 -0300

    Finalizar authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,10 +7,16 @@ const authController = require('../controllers/authController');
 // Define as rotas de autenticação:
 
 // o formulário de registro enviará os dados para esta rota
-router.post('login', authController.registerUser);
+router.post('register', authController.registerUser);
 
 // o formulário de login enviará os dados para esta rota
+router.post('login', authController.loginUser);
 
 //  rota para a página de acesso ao painel de usuário
+router.get('painel', authController.getUserPanel);
 
 // rota de logout
+router.get('/logout', authController.logoutUser);
+
+// Exporta o router para o server.js
+module.exports = router;
