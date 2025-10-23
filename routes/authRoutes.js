0e89ac1b483a6a01ca591c07d0fb4d159daee37a
Luiz Luna <luizluna.dev@gmail.com>
2025-10-23 12:50:26 -0300

    refatorar rotas de autenticação

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
-const authController = require('../controllers/authController');
-const User = require('../models/User');
 
 // Importa o authController
 const authController = require('../controllers/authController');
@@ -10,16 +8,38 @@ const authController = require('../controllers/authController');
 // Define as rotas de autenticação:
 
 // o formulário de registro enviará os dados para esta rota
-router.post('register', authController.registerUser);
+router.post(
+    '/register',
+    [
+        // regra 1: o campo 'nome' não pode estar vazio.
+        body('nome')
+            .trim() // remove espaços em branco
+            .notEmpty().withMessage('O campo nome é obrigatório'),
+
+        // regra 2: o campo 'email' deve ser um e-mail válido.
+            body('email')
+                .isEmail().withMessage('Por favor, insira um e-mail válido'),
+
+        //regra 3: o campo 'senha' deve ter no mínimo 8 caracteres
+        body('senha')
+            .isLength({ min: 8}).withMessage('A senha deve ter no mínimo 8 caracteres.')
+    ],
+    authController.postRegister
+);
 
 // o formulário de login enviará os dados para esta rota
-router.post('login', authController.loginUser);
-
-//  rota para a página de acesso ao painel de usuário
-router.get('painel', authController.getUserPanel);
+router.post(
+    '/login',
+    [
+        // Validações simples
+        body('email').isEmail().withMessage('Por favor, insira um e-mail válido.'),
+        body('senha').notEmpty().withMessage('A senha é obrigatória.')
+    ],
+    authController.postLogin
+);
 
 // rota de logout
-router.get('/logout', authController.logoutUser);
+router.get('/logout', authController.getLogout);
 
 // Exporta o router para o server.js
 module.exports = router;
